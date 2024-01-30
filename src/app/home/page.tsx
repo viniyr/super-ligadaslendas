88ef4f5e945e3a26cfa97c498b6e07afc31ae466
vini <ripvny@gmail.com>
2024-01-30 13:44:27 -0300

    logs

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -97,6 +97,7 @@ async function getPlayers() {
             method: 'GET'
         }
         const users = await (await fetch(url, fetchOptions)).json();
+        console.log(users)
         return users.response
     } catch (error) {
         console.log(error)
