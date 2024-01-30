293213ba712da054f35fb7effd832b5ade6704fe
vini <ripvny@gmail.com>
2024-01-30 17:13:04 -0300

    fix mdal

--- src/components/shared/modals/DialogContainer.tsx
+++ src/components/shared/modals/DialogContainer.tsx
@@ -48,7 +48,7 @@ export function DialogContainer({ children, isOpen, closeModal }: DialogContaine
                             leaveTo="-translate-x-full opacity-0"
                         >
                             <HeadlessDialog.Panel
-                                className="w-screen h-full lg:min-w-[600px] transform overflow-hidden rounded-lg bg-primary-color backdrop-blur-2xl bg-opacity-50 px-4 lg:px-6 py-12 lg:p-12 text-left align-middle shadow-2xl transition-all bg-gradient-139 from-black/25 to-transparent border-[1px] border-lol-red/50">
+                                className="w-screen h-full md:w-fit lg:min-w-[600px] transform overflow-hidden rounded-lg bg-primary-color backdrop-blur-2xl bg-opacity-50 px-4 lg:px-6 py-12 lg:p-12 text-left align-middle shadow-2xl transition-all bg-gradient-139 from-black/25 to-transparent border-[1px] border-lol-red/50">
 
 
                                 <Button onClick={() => internalCloseModal()} color='secondary' customStyle='absolute right-2 top-2 w-[42px] h-[42px] font-bold text-lg rounded-lg shadow-md drop-shadow-md !bg-transparent !border-0'>
